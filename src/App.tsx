2e601db67b4b6a989cd2b401ac2face6d6633d4c
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2024-01-16 21:33:43 +0600

    layout and router setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import MainLayout from "./components/layout/MainLayout";
+
 function App() {
   return (
     <div>
-      <h1>Ph-University Managment</h1>
+      <MainLayout />
     </div>
   );
 }
---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,73 @@
+import { Layout, Menu, MenuProps } from "antd";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: "Dashboard",
+  },
+  {
+    key: "2",
+    label: "Profile",
+  },
+  {
+    key: "3",
+    label: "Setting",
+  },
+];
+
+const MainLayout = () => {
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "4rem",
+            fontSize: "20px",
+            fontWeight: "bold",
+          }}
+        >
+          <span>PH-University</span>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            content
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default MainLayout;
