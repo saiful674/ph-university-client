dcea631ec55a0fbc94bf5a6cb1868d04074d5f6b
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2024-01-25 19:49:29 +0600

    custom base query added for geting access token after expire by refresh token and after refresh token's expire logout the user

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { logout, setUser } from "../features/auth/authSlice";
 import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
@@ -9,11 +10,42 @@ const baseQuery = fetchBaseQuery({
     if (token) {
       headers.set("authorization", `${token}`);
     }
+
+    return headers;
   },
 });
 
+const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result.error?.status === 401) {
+    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+    const data = await res.json();
+
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).auth.user;
+      api.dispatch(
+        setUser({
+          user,
+          token: data.data.accessToken,
+        })
+      );
+      console.log(data);
+    } else {
+      api.dispatch(logout());
+    }
+
+    result = await baseQuery(args, api, extraOptions);
+  }
+
+  return result;
+};
+
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery,
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
